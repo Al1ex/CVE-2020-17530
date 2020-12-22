2ecf245505f72eac7ee4fcf1efeeb819910b0f67
Al1ex <38161463+Al1ex@users.noreply.github.com>
2020-12-22 23:27:50 +0800

    update environment

---
+++ CVE-2020-17530/src/main/java/org/heptagram/action/IndexAction.java
@@ -0,0 +1,17 @@
+package org.heptagram.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+public class IndexAction  extends ActionSupport {
+
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String Test(){
+        return SUCCESS;
+    }
+}
---
+++ CVE-2020-17530/src/main/webapp/S2061.jsp
@@ -0,0 +1,13 @@
+<%@ page
+        language="java"
+        contentType="text/html; charset=UTF-8"
+        pageEncoding="UTF-8" %>
+<%@ taglib prefix="s" uri="/struts-tags" %>
+<html>
+<head>
+    <title>S2061</title>
+</head>
+<body>
+<s:a id="%{id}">SimpleTest</s:a>
+</body>
+</html>
